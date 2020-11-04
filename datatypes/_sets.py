75585466665f9be94969eb2e3239435ef3bfbf6d
Madi <72963376+Madi-S@users.noreply.github.com>
2020-11-04 21:40:46 +0600

    Update _sets.py

--- datatypes/_sets.py
+++ datatypes/_sets.py
@@ -1,1 +1,20 @@
 
+import redis
+
+mylist = list() # To make sure you understand everything corresponding commands in redis for lists will be shown with python lists' commands
+
+
+# In this tutorial we are gonna cover following commands for lists:
+
+# Lists
+# lpush & rpush
+# lpushx & rpushx
+# lpop & rpop
+# lindex 
+# llen 
+# lrem
+# lset
+# lrange
+# delete
+
+with redis.Redis(host='127.0.0.1', port=6379) as client:
