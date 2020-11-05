e74a77728c7cf93e72c020054c348ef7d3843708
Madi <72963376+Madi-S@users.noreply.github.com>
2020-11-05 09:17:19 +0600

    Update _sorted_sets.py

--- datatypes/_sorted_sets.py
+++ datatypes/_sorted_sets.py
@@ -1,1 +1,6 @@
+import redis
+
+# Unfortunately, python does not have such datatype,, so we are unable to compare similar commands.
+# Redis sorted sets are collection of non-repeating strings, where each string is acossiated with a score and index that are used to order sortes sets.
+# In this tutorial we are gonna cover following commands for sorted sets:
 
