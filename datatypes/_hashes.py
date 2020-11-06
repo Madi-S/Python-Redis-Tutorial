405a6987bd6379b61bf37dde65db8d9ef6d6e03a
Madi <72963376+Madi-S@users.noreply.github.com>
2020-11-06 11:00:00 +0600

    Update _hashes.py

--- datatypes/_hashes.py
+++ datatypes/_hashes.py
@@ -20,11 +20,12 @@ import redis
 
 
 with redis.Redis(host='127.0.0.1', port=6379) as client:
-    client.hset('Car', 'Price', '50000$')
-    # structure is like client.hset(hash_name, key and value or mapping=dict)
-    data = {'Price': '15000$'}
-    client.hset('Car', mapping=data)                # a single key-value (dict) pair must be passed as an argument 
+    client.hset('Car', 'Price', '50000$')           # Structure be like client.hset(hash_name, key and value or mapping=dict)
     
+    data = {'Price': '15000$'}
+    client.hset('Car', mapping=data)                # Single key-value (dict) pair must be passed as an argument 
+    
+    # Get all keys or/and values and length:
     car_data = client.hgetall('Car')
     keys = client.hkeys('Car')
     values = client.hvals('Car')
@@ -32,34 +33,37 @@ with redis.Redis(host='127.0.0.1', port=6379) as client:
     
     print(car_data, keys, values, length)           # {b'Price': b'50000$'} [b'Price'] [b'50000$'] 1
     print(client.hget('Car', 'Price'))              # b'50000$
-
+    
+    # Check if given key exists in a hash:
     if not client.hexists('Car', 'Speed'):
         print('Speed of the car is not given')
         client.hset('Car', 'Speed (km/h)', 270)
-
+    
+    # Increment value in a hash:
     client.hincrby('Car', 'Speed (km/h)', 30)
     client.hincrbyfloat('Car', 'Speed (km/h)', -2.5)
     print(client.hget('Car', 'Speed (km/h)'))       # 270 + 30 - 2.5 = 297.5
 
-    # Add key-value pair if hash with given name does not exists
+    # Add key-value pair if hash with given name does not exists:
     client.hsetnx('Person', 'Name', 'John')
-    # Thus this key-value pair will not ovewrite existing key-value pair in Person hash
-    client.hsetnx('Person', 'Name', 'Maximilian')
-    print(client.hget('Person', 'Name'))  # b'John'
+    client.hsetnx('Person', 'Name', 'Maximilian')   # Thus this key-value pair will not ovewrite existing key-value pair in Person hash
+    print(client.hget('Person', 'Name'))            # b'John'
 
     person_data = {'Married?': 'Yes',
                    'Homeless?': 'No',
                    'Socially active?': 'No'}
-    # A ditionary with multiple key-value pairs can be used
-    client.hmset('Person', person_data)
+    
+    # Set multiple key-value pairs to a hash:
+    client.hmset('Person', person_data)             # A dictionary with multiple key-value pairs can be used in hmset as mapping
 
-    # To get multiple values
+    # Gett multiple values from a hash:
     print(client.hmget('Person', 'Married?', 'Homeless?'))  # [b'Yes', b'No']
 
-    # To delete key-value pair from a hash
+    # Delete key-value pair from a hash:
     client.hdel('Person', 'Name')
-    print(client.hget('Person', 'Name')) # None
-
+    print(client.hget('Person', 'Name'))            # None
+    
+    # On deleting a stored key, redis returns True if it was deleted successsfully
     if client.delete('Car'):
         print('Hash "Car" was deleted')
 
